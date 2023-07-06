6e556e5f0ebcc03c332ef1e1cfe82b8eaf4e4496
saurabh singh <singhsaurabh1905@gmail.com>
2023-07-06 13:36:29 +0530

    completed hero header and footer

---
+++ components/SearchBar.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import React from 'react'
+
+const SearchBar = () => {
+
+const heandleSearch= ()=>{}
+
+  return (
+    <form className='searchbar' onSubmit={heandleSearch}>
+
+    </form>
+  )
+}
+
+export default SearchBar
